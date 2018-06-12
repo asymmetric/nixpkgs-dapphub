a2279c9fc061f74460f359d5f448f2a8665834a0
Lorenzo Manacorda <lorenzo@mailbox.org>
2018-06-12 14:49:28 +0200

    CI: Workaround lack of macOS support in cachix
    Since the Travis nix installer does a multi-user install, it's necessary
    to trust the current user in order for it to be able to set
    substituters.

--- scripts/install-dapphub-cache.sh
+++ scripts/install-dapphub-cache.sh
@@ -9,3 +9,12 @@ binary-caches = https://cache.nixos.org https://d121dybo9yhl5h.cloudfront.net
 trusted-binary-caches = https://cache.nixos.org https://d121dybo9yhl5h.cloudfront.net
 trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= d121dybo9yhl5h.cloudfront.net-1:IOpHdUBT2b3fIRb/TBm+V8Y8eEj52fStcllzD6TFYyU=
 EOF
+
+
+# Workaround until proper macOS support is added to cachix.
+# See https://github.com/cachix/cachix/issues/26
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  echo "trusted-users = root $USER" | sudo tee -a /etc/nix/nix.conf
+
+  sudo launchctl kickstart -k system/org.nixos.nix-daemon
+fi
